4c5c3b45cafe0e2375c9209a3960260872c9ebc8
Bharat Kumar <bharattitop@gmail.com>
2017-08-12 11:42:11 +0530

    Just another test

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-num=int(raw_input("Enter num to convert in hexadecimal: "))
-i=0
-while(num>0):
-    if num<10:
-        print(num)
-    elif num==10:
-        print("A")
-    elif num==11:
-        print "B"
-    elif num==12:
-        print "C"
-    elif num==13:
-        print "D"
-    elif num==14:
-        print "E"
-    elif num==15:
-        print "F"
-    else:
-        print "I don't know."
+list = []
+for i in range(2000,3000):
+    if i%7==0 and not(i%5==0):
+        list.append(str(i))
+print ','.join(list)
+print(len(list))
