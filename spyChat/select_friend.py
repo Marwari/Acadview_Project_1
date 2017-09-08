2537a4840e7ecfda1a8ff752bfeb052cc07997bb
Marwari <bharattitop@gmail.com>
2017-09-09 03:19:52 +0530

    Project has been completed.

--- spyChat/select_friend.py
+++ spyChat/select_friend.py
@@ -1,10 +1,28 @@
-from globals import friends
+# importing older friend if exist
+from spy_details import friends
 
+#from globals import friends
+
+# importing termcolor and colorama
+from termcolor import colored
+from colorama import init
+
+#initializing colorama
+init()
+
+# function to select friend from friend list.
 def select_friend():
+
+    # indexing position of friend.
     counter = 1
+
+    # to select a friend with indexing
     for friend in friends:
-        print str(counter) + ". " + friend['name'] + "Age : " + str(friend['age'])
+        print str(counter) + ". " + friend.name + "Age : " + str(friend.age)
+
         counter = counter + 1
 
-    result = int(raw_input("Select from the list : "))
+    # ask user which friend he wants to select to chat with
+    result = int(raw_input(colored("Select from the list : ", 'magenta')))
+    # result the selected friend to perform action
     return result - 1
